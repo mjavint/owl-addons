b5c5c908f60ff19d5a5b073c5d1338404f3fa0c2
mjavint <mjavint@gmail.com>
2024-07-19 20:35:37 -0400

    [ADD] Add Owl in website

---
+++ owl_website/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Website with OWL',
+    'version': '17.0.1.0.0',
+    'summary': """ Website with OWL """,
+    'author': 'mjavint',
+    'website': 'https://www.youtube.com/@devtoolschool',
+    'category': 'Website',
+    'depends': ['base', 'web', 'website'],
+    "data": ["views/templates.xml"],
+    'assets': {
+        'web.assets_frontend': [
+            'owl_website/static/src/components/item/item.js',
+            'owl_website/static/src/components/item/item.xml',
+        ]
+    },
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
